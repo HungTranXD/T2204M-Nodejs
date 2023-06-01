6bdfbe019b6cec289569e194cf3622dae866d811
HungTranXD <105595047+HungTranXD@users.noreply.github.com>
2023-06-01 21:04:20 +0700

    session 3
    authentication function

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 
+// Buoi 3: Luu user vao session
+const session = require('express-session');
+app.use(session({
+  secret: 'your-secret-key', // Add a secret key to sign the session ID cookie
+  resave: false,
+  saveUninitialized: false
+}));
+// Set up a middleware to fetch `user` from the session
+app.use((req, res, next) => {
+    // Fetch `user` from the session and make it available globally
+    res.locals.user = req.session.user;
+    next();
+  });
+
+
+  
 app.get("/", (req, res) => {
     const Product = require("./src/models/product");
     Product.find({})
@@ -43,6 +59,10 @@ app.use("/categories", categoryRoutes);
 // CRUD for brands
 const brandRoutes = require("./src/routes/brand.route");
 app.use("/brands", brandRoutes);
+
+// Auth
+const authRoutes = require("./src/routes/auth.route");
+app.use("/auth", authRoutes);
 
 
 
---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: [6, "Ten phai co do dai thoi thieu la 6"],
+        maxLength: 255
+    },
+    email: {
+        type: String,
+        required: true,
+        minLength: [6, "Email phai co do dai toi thieu la 6"],
+        maxLength: 100,
+        unique: true,
+        validate: {
+            validator: (v) => {
+                const emailPattern = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+                return v.match(emailPattern);
+            },
+            message: (t) => `${t.value} khong phai la email`
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minLengtb: 6,
+        maxLength: 255,
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
